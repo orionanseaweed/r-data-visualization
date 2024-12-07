9b49ce325a8613864f6cc7db3a113a7acdb990a9
Kane <concealedcharacterx@gmail.com>
2024-12-07 15:42:24 +0700

    Update 1-data-visualization.R

--- whole-game/1-data-visualization.R
+++ whole-game/1-data-visualization.R
@@ -8,7 +8,7 @@ str(penguins)
 
 # 1.2.5 Exercise 8: Recreate the visualization
 
-ggplot(penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
-geom_point(aes(color=bill_depth_mm)) +
-geom_smooth()
+> ggplot(penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
++ geom_point(aes(color=bill_depth_mm)) +
++ geom_smooth()
 
