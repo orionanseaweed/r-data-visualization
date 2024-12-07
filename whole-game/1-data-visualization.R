5a34e8fadf33da1b929bea139596a95a1cb3bacb
Kane <concealedcharacterx@gmail.com>
2024-12-07 15:41:52 +0700

    Update and rename data-visualization.R to 1-data-visualization.R
    add 1.2.5 exercise 8

---
+++ whole-game/1-data-visualization.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+library(palmerpenguins)
+library(ggthemes)
+
+str(penguins)
+
+# 1.2.5 Exercise 8: Recreate the visualization
+
+ggplot(penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
+geom_point(aes(color=bill_depth_mm)) +
+geom_smooth()
+
--- whole-game/data-visualization.R
+++
@@ -1,6 +0,0 @@
-install.packages("tidyverse")
-library(tidyverse)
-
-ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
-  geom_point(aes(color = species, shape = species)) +
-  facet_wrap(~island)
