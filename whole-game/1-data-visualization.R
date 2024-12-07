e5d02662ec7598f8c70f87028d744b0194680450
Kane <concealedcharacterx@gmail.com>
2024-12-07 15:47:49 +0700

    Update 1-data-visualization.R

--- whole-game/1-data-visualization.R
+++ whole-game/1-data-visualization.R
@@ -1,14 +1,25 @@
 install.packages("tidyverse")
-library(tidyverse)
 
+library(tidyverse)
 library(palmerpenguins)
 library(ggthemes)
 
+# take a look at the data set
+
 str(penguins)
 
-# 1.2.5 Exercise 8: Recreate the visualization
+# 1.2.5 Exercise 8: Recreate the visualization.
 
-> ggplot(penguins, aes(x=flipper_length_mm, y=body_mass_g)) +
-+ geom_point(aes(color=bill_depth_mm)) +
-+ geom_smooth()
+ggplot(
+  penguins,
+  aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(aes(color = bill_depth_mm)) +
+  geom_smooth()
 
+# or
+
+penguins |>
+ggplot(aes(x = flipper_length_mm, y = body_mass_g)) + 
+geom_point(aes(color = bill_depth_mm)) +
+geom_smooth()
