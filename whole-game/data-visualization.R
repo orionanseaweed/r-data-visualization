38442accc99d397cd3cdee308d455be5d06a2b4a
Kane <concealedcharacterx@gmail.com>
2024-12-07 14:33:57 +0700

    Update data-visualization.R

--- whole-game/data-visualization.R
+++ whole-game/data-visualization.R
@@ -1,3 +1,6 @@
+install.packages("tidyverse")
+library(tidyverse)
+
 ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
   geom_point(aes(color = species, shape = species)) +
   facet_wrap(~island)
