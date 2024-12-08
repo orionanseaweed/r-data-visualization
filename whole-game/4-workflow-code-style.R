7ede5936968749f5644dce55fc1b4e973da7d3f6
Kane <concealedcharacterx@gmail.com>
2024-12-08 10:24:24 +0700

    Update 4-workflow-code-style.R

--- whole-game/4-workflow-code-style.R
+++ whole-game/4-workflow-code-style.R
@@ -1,4 +1,11 @@
-# requirement: restyle the following pipelines following the guidelines
+# requirements
+
+install.packages("tidyverse")
+
+library(tidyverse)
+library(nycflights13)
+
+# 4.6 exercise 1: restyle the following pipelines following the guidelines
 
 flights|>filter(dest=="IAH")|>group_by(year,month,day)|>summarize(n=n(),
 delay=mean(arr_delay,na.rm=TRUE))|>filter(n>10)
@@ -7,5 +14,21 @@ flights|>filter(carrier=="UA",dest%in%c("IAH","HOU"),sched_dep_time>
 0900,sched_arr_time<2000)|>group_by(flight)|>summarize(delay=mean(
 arr_delay,na.rm=TRUE),cancelled=sum(is.na(arr_delay)),n=n())|>filter(n>10)
 
+# 4.6 exercise 1 solution
+
+flights |>
+  filter(
+    carrier == "UA",
+    dest %in% c("IAH", "HOU"),
+    sched_dep_time > 0900,
+    sched_arr_time < 2000
+  ) |>
+  group_by(flight) |>
+  summarize(
+    delay = mean(arr_delay, na.rm = TRUE),
+    cancelled_sum(is.na(arr_delay)),
+    n = n()
+  ) |>
+  filter(n > 10)
 
 
