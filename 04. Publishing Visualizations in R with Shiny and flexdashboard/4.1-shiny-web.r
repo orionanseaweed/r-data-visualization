c6a2e954207edcfeb19073dde7cc145eff772202
Kane <concealedcharacterx@gmail.com>
2025-02-04 23:16:25 +0700

    Enhance workflow.

--- 04. Publishing Visualizations in R with Shiny and flexdashboard/4.1-shiny-web.r
+++ 04. Publishing Visualizations in R with Shiny and flexdashboard/4.1-shiny-web.r
@@ -3,9 +3,9 @@ library(tidyverse)
 
 ##### Import Data
 
-dat<-read_csv(url("https://www.dropbox.com/s/uhfstf6g36ghxwp/cces_sample_coursera.csv?raw=1"))
-dat<- dat %>% select(c("pid7","ideo5"))
-dat<-drop_na(dat)
+dat <- read_csv(url("https://www.dropbox.com/s/uhfstf6g36ghxwp/cces_sample_coursera.csv?raw=1"))
+dat <- dat %>% select(c("pid7", "ideo5"))
+dat <- drop_na(dat)
 
 ui<- fluidPage(
   
